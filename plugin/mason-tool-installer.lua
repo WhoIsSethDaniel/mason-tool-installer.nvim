b005ab47c32cae6e39a8668fe1c73c542de070f5
Seth Daniel <seth@sethdaniel.org>
2022-07-21 16:43:23 -0500

    start on BufEnter instead of VimEnter; perform only once

--- plugin/mason-tool-installer.lua
+++ plugin/mason-tool-installer.lua
@@ -1,3 +1,7 @@
-vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+local augroup = vim.api.nvim_create_augroup('MasonToolInstaller', { clear = true })
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+  group = augroup,
+  pattern = { '*' },
+  once = true,
   callback = require('mason-tool-installer').check_install,
 })
